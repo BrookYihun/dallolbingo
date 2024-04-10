4ab70c3da24faffee44c1a02165564be7b537da6
BrookYihun <brookyihun@gmail.com>
2024-04-10 19:03:33 +0300

    13th

--- dallolbingo/settings.py
+++ dallolbingo/settings.py
@@ -142,7 +142,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("5.75.175.113", 6379)],
+            "hosts": [("127.0.0.1", 6379)],
         },
     },
 }

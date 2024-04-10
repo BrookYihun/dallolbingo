f2bbb49b4dad03165f195bb65760319431d54746
BrookYihun <brookyihun@gmail.com>
2024-04-10 16:44:38 +0300

    4th

--- dallolbingo/asgi.py
+++ dallolbingo/asgi.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
@@ -16,6 +17,7 @@ from channels.auth import AuthMiddlewareStack
 from game import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dallolbingo.settings')
+django.setup()
 
 application = ProtocolTypeRouter({
     'http':get_asgi_application(),

c6791fe866e1d88a5444e102f311f903b18d696e
BrookYihun <brookyihun@gmail.com>
2024-04-10 22:50:35 +0300

    18th

--- account/urls.py
+++ account/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('login/',views.login_view,name="login"),
     path('logout/',views.logout_view,name="logout"),
     path('register/',views.register_view,name="register"),
+    path('register_tel/',views.register_tel_view,name="register_tel"),
+    path('login_tel/',views.login_tel_view,name="login_tel")
 ]

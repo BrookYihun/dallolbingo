46f05104cbaa8b175835136c83fd406c0bb8773b
BrookYihun <brookyihun@gmail.com>
2025-04-22 08:41:38 +0300

    jkesb

--- bingo/settings.py
+++ bingo/settings.py
@@ -74,7 +74,10 @@ ASGI_APPLICATION = 'bingo.asgi.application'
 # Use in-memory channel layer for local dev
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer"
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
     },
 }
 

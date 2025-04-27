61cf0c4661c518950ce0534cc1f62098943731b9
BrookYihun <brookyihun@gmail.com>
2025-04-27 12:28:20 +0300

    rout

--- cashier/routing.py
+++ cashier/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/game/(?P<shop>\w+)/(?P<game_id>\w+)/$', consumers.CashierGameConsumer.as_asgi()),
+    re_path(r'backup/ws/game/(?P<shop>\w+)/(?P<game_id>\w+)/$', consumers.CashierGameConsumer.as_asgi()),
 ]
